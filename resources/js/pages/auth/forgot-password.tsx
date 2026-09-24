e86914183041e594fcef72abbebf0fbb183d9833
jkian <slyrics443@gmail.com>
2026-09-24 17:23:08 +0800

    remove password.email problem

--- resources/js/pages/auth/forgot-password.tsx
+++ resources/js/pages/auth/forgot-password.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { login } from '@/routes';
-import { email } from '@/routes/password';
+// import { email } from '@/routes/password';
 
 export default function ForgotPassword({ status }: { status?: string }) {
     return (
@@ -21,7 +21,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
             )}
 
             <div className="space-y-6">
-                <Form {...email.form()}>
+                {/* <Form {...email.form()}>
                     {({ processing, errors }) => (
                         <>
                             <div className="grid gap-2">
@@ -52,7 +52,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
                             </div>
                         </>
                     )}
-                </Form>
+                </Form> */}
 
                 <div className="space-x-1 text-center text-sm text-muted-foreground">
                     <span>Or, return to</span>
