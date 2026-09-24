22e76442cadde54901f32b9dce920e2908ad8b0f
jkian <slyrics443@gmail.com>
2026-09-24 20:16:46 +0800

    bring back the sutpid quantity

--- app/Http/Controllers/ReliefPackController.php
+++ app/Http/Controllers/ReliefPackController.php
@@ -74,7 +74,7 @@ class ReliefPackController extends Controller
             PackReceipt::create([
                 'relief_pack_id' => $reliefPack->id,
                 'source_name' => $validated['source_name'],
-                // 'quantity_received' => $validated['quantity_received'],
+                'quantity_received' => $validated['quantity_received'],
                 'date_received' => $validated['date_received'],
                 'received_by' => $request->user()->id,
             ]);
